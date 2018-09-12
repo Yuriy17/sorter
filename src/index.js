799407b76f02200bff3df7070a99e059566af538
Valery <arkhangel4949@gmail.com>
2018-09-12 16:44:55 +0300

    first

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 class Sorter {
+
+  var arr = [];
   constructor() {
+
     // your implementation
   }
 
   add(element) {
+    arr.push(element);
     // your implementation
   }
 
@@ -12,14 +16,27 @@ class Sorter {
   }
 
   get length() {
+    return arr.length
     // your implementation
   }
 
   toArray() {
+    var str;
+    for (i=0; i<arr.length; i++){
+      if(i=0){
+        str='['+arr[i]+', ';
+      } else if (i=arr.length-1){
+        str=arr[i]+']';
+      }else{
+        str=arr[i]+', ';
+      }
+    }
+    return str;
     // your implementation
   }
 
   sort(indices) {
+    indices
     // your implementation
   }
 
