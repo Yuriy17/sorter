e79ae90dee662fbfd14a0ec05b1e22a0592df1e3
Valery <arkhangel4949@gmail.com>
2018-09-13 17:21:12 +0300

    beta

--- src/index.js
+++ src/index.js
@@ -1,47 +1,62 @@
 class Sorter {
+  
 
-  var arr = [];
   constructor() {
-
+    this.arr = new Array();
     // your implementation
   }
 
   add(element) {
-    arr.push(element);
+
+    this.arr.push(element);
     // your implementation
   }
 
   at(index) {
+    return this.arr[index];
     // your implementation
   }
 
   get length() {
-    return arr.length
+    return this.arr.length;
     // your implementation
   }
 
   toArray() {
-    var str;
-    for (i=0; i<arr.length; i++){
-      if(i=0){
-        str='['+arr[i]+', ';
-      } else if (i=arr.length-1){
-        str=arr[i]+']';
-      }else{
-        str=arr[i]+', ';
-      }
-    }
-    return str;
+    return this.arr;
     // your implementation
   }
 
   sort(indices) {
-    indices
+
+
+    if(indices.length!==1){
+      if (indices.length!==this.arr.length){
+        
+        var x,y
+         = [], z=indices[0];
+
+        for( var i=0; i<indices.length;i++){
+          x.push(this.arr[indices[i]]);
+        }
+
+
+
+      }else{
+        this.arr=this.arr.sort(function(a, b){return a-b});
+      }
+    }
+
     // your implementation
   }
 
   setComparator(compareFunction) {
     // your implementation
+  }
+
+  compareNumeric(a, b) {
+    if (a > b) return 1;
+    if (a < b) return -1;
   }
 }
 
