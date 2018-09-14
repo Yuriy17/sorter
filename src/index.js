cd5f0e18a68c74253b3762260514f76a5b52eabb
Valery <arkhangel4949@gmail.com>
2018-09-14 16:06:02 +0300

    second

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ class Sorter {
 
   constructor() {
     this.arr = new Array();
+    this.comparator;
     // your implementation
   }
 
@@ -28,36 +29,45 @@ class Sorter {
   }
 
   sort(indices) {
-
-
-    if(indices.length!==1){
-      if (indices.length!==this.arr.length){
-        
-        var x,y
-         = [], z=indices[0];
-
-        for( var i=0; i<indices.length;i++){
-          x.push(this.arr[indices[i]]);
-        }
-
-
-
-      }else{
-        this.arr=this.arr.sort(function(a, b){return a-b});
-      }
+    var n=this.arr.length,l=indices.length;
+    switch (compareFunction) {
+      case AGE_COMPARATOR:
+        this.comparator=age;
+        break;
+      case REVERSE_COMPARATOR:
+        this.comparator=comparator;
+        break;
+      case STRINGIFY_COMPARATOR:
+        this.comparator=stringify;
+        break;
+      default:
+        if(l!==1){
+          if (l!==n){
+            indices=indices.sort(function(a, b){return a-b});
+            for (var i = 0; i < l-1; i++)
+              { for (var j = 0; j < l-1-i; j++)
+                 { if (this.arr[indices[j+1]] < this.arr[indices[j]])
+                    { var t = this.arr[indices[j+1]];
+                     this.arr[indices[j+1]] = this.arr[indices[j]];
+                     this.arr[indices[j]] = t; }
+                 }
+              }  
+          }else{
+            this.arr=this.arr.sort(function(a, b){return a-b});
+          }
+        };
     }
+
 
     // your implementation
   }
 
   setComparator(compareFunction) {
+   this.comparator=compareFunction;
+
     // your implementation
   }
 
-  compareNumeric(a, b) {
-    if (a > b) return 1;
-    if (a < b) return -1;
-  }
 }
 
 module.exports = Sorter;
